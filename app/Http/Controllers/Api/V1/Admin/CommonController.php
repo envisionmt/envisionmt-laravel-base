29a3c1f0a44ef6b2c5a1d56d3f30c5d129f52e61
thanhle <thanhle09t2bkdn@gmail.com>
2022-10-09 11:00:14 +0700

    logs

---
+++ app/Http/Controllers/Api/V1/Admin/CommonController.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Http\Controllers\Api\V1\Admin;
+
+
+use App\Http\Controllers\Api\ApiController;
+
+class CommonController extends ApiController
+{
+
+    public function __construct()
+    {
+    }
+
+    public function create() {
+
+    }
+}
